82dfd9a50d686191f5acf64b301c8a4673ce70b5
Vaishnavi-N-08 <vaishnavinarkhede8@gmail.com>
2021-12-31 16:48:29 +0530

    model

--- compressor/admin.py
+++ compressor/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from compressor.models import Data
 # Register your models here.
+
+class DataModelAdmin(admin.ModelAdmin):
+    model = Data
+    list_display = [
+        'id',
+        'encoded_string'
+        ]
+
+
+admin.site.register(Data, DataModelAdmin)
--- compressor/models.py
+++ compressor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Data(models.Model):
+    id = models.AutoField(primary_key=True)
+    encoded_string = models.TextField(null=False, blank=False)
+
+    def _str_(self):
+        return str(self.pk)
